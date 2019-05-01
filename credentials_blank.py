790d1baddad72cc72baacf047b43b5e6e7ba8754
itsmuzzle <89t0asty@gmail.com>
2019-05-01 23:24:46 +0100

    Create credentials_blank.py

---
+++ credentials_blank.py
@@ -0,0 +1,10 @@
+login = {
+    'dashboard_login_url': 'http://autostock.lkqcoatings.com/#/login',
+    'dashboard_search_url': 'http://autostock.lkqcoatings.com/#/usage-by-product-operative/YOURID/',
+    'dashboard_username': 'yourUsername',
+    'dashboard_password': 'yourPassword',
+    'products_to_search': ['MMM.06311', 'MMM.51099', 'MMM.51174', 'MMM.16026', 'MMM.16740'],
+    'date_search_from': '1/4/2019',
+    'date_search_until': '30/4/2019',
+    'path_to_chrome_driver': 'C:\\Path\\To\\Driver\\chromedriver.exe'
+}
